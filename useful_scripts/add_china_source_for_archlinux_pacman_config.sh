4eddbcb508fa9ced09b57bde5e3cd2a213d0b6a2
XiaoLi <35646108+xiaolitongxue666@users.noreply.github.com>
2021-11-13 13:31:27 +0800

    Update add_china_source_for_archlinux_pacman_config.sh

--- useful_scripts/add_china_source_for_archlinux_pacman_config.sh
+++ useful_scripts/add_china_source_for_archlinux_pacman_config.sh
@@ -2,11 +2,32 @@
 
 set -x 
 
-cat >/etc/pacman.conf <<EOL
+# mirrorlist
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 
-[archlinuxcn]
-SigLevel = Optional TrustedOnly
-Server = https://mirrors.ustc.edu.cn/archlinuxcn/\$arch
+cat >/etc/pacman.d/mirrorlist <<EOL
+# 清华大学
+Server = https://mirrors.tuna.tsinghua.edu.cn/archlinux/\$repo/os/\$arch
+## 163
+Server = http://mirrors.163.com/archlinux/\$repo/os/\$arch
+## aliyun
+Server = http://mirrors.aliyun.com/archlinux/\$repo/os/\$arch
+
 EOL
 
+# pacman.conf
+cp /etc/pacman.conf /etc/pacman.conf.backup
+
+cat >/etc/pacman.conf <<EOL
+[options]
+Architecture = auto
+[archlinuxcn]
+SigLevel = Optional TrustAll
+Server = https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/\$arch
+EOL
+
+# show config
+cat /etc/pacman.d/mirrorlist
+
 cat /etc/pacman.conf
+
