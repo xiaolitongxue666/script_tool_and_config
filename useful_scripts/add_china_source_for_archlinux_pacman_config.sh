f568fe5fbcaa9e446ec5ab419b511fb1e1aaee67
XiaoLi <35646108+xiaolitongxue666@users.noreply.github.com>
2021-11-13 17:56:57 +0800

    Update add_china_source_for_archlinux_pacman_config.sh

--- useful_scripts/add_china_source_for_archlinux_pacman_config.sh
+++ useful_scripts/add_china_source_for_archlinux_pacman_config.sh
@@ -20,14 +20,33 @@ cp /etc/pacman.conf /etc/pacman.conf.backup
 
 cat >/etc/pacman.conf <<EOL
 [options]
+HoldPkg     = pacman glibc
 Architecture = auto
+CheckSpace
+SigLevel    = Required DatabaseOptional
+LocalFileSigLevel = Optional
+
+[core]
+SigLevel = PackageRequired
+Include = /etc/pacman.d/mirrorlist
+
+[extra]
+SigLevel = PackageRequired
+Include = /etc/pacman.d/mirrorlist
+
+[community]
+SigLevel = PackageRequired
+Include = /etc/pacman.d/mirrorlist
+
 [archlinuxcn]
 SigLevel = Optional TrustAll
 Server = https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/\$arch
 EOL
 
-# show config
+# Show config
 cat /etc/pacman.d/mirrorlist
 
 cat /etc/pacman.conf
 
+# Update pacman source
+pacman -Syy
