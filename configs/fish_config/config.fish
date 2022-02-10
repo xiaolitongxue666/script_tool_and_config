d0c7a94d5eaad091197542e991edfa24cd90cd8e
XiaoLi <35646108+xiaolitongxue666@users.noreply.github.com>
2022-02-10 13:57:29 +0800

    Update config.fish

--- configs/fish_config/config.fish
+++ configs/fish_config/config.fish
@@ -2,16 +2,15 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# Add by xiaoli
+# Add alias
 alias h_proxy='export http_proxy=http://127.0.0.1:1087;export https_proxy=http://127.0.0.1:1087'
 alias unset_h='unset http_proxy; unset https_proxy'
 alias c_google='curl www.google.com'
-
 alias cat='bat --style=plain'
-
 alias l='exa'
 alias la='exa -a'
 alias ll='exa -lah'
 alias ls='exa --color=auto'
 
-
+# Add cargo path : fish version >= 3.3.1
+fish_add_path ~/.cargo/bin
