e97009de681e52e86c9b9a3ea6648750fd79cf44
XiaoLi <35646108+xiaolitongxue666@users.noreply.github.com>
2022-02-11 17:00:04 +0800

    Update auto_install_dwm_for_centos_stream.sh

--- useful_scripts/auto_install_dwm_for_centos_stream.sh
+++ useful_scripts/auto_install_dwm_for_centos_stream.sh
@@ -7,7 +7,13 @@ dnf install libX11-devel libXt-devel libXft-devel libXext-devel libXp-devel libX
 
 # Build and install
 git clone https://git.suckless.org/dwm
-
 cd dwm
-
 make clean install
+
+# Install st
+cd ..
+wget https://dl.suckless.org/st/st-0.8.5.tar.gz
+tar -zxvf st-0.8.5.tar.gz
+cd st-0.8.5
+make clean isntall
+tic -sx st.info
