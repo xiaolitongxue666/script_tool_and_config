774a4b0d1aafd7558dd99a0e216cefd326e6c353
XiaoLi <35646108+xiaolitongxue666@users.noreply.github.com>
2022-02-16 21:03:20 +0800

    Update config.fish

--- configs/fish_config/config.fish
+++ configs/fish_config/config.fish
@@ -5,7 +5,7 @@ end
 # Add alias
 alias h_proxy='export http_proxy=http://127.0.0.1:1087;export https_proxy=http://127.0.0.1:1087'
 alias unset_h='unset http_proxy; unset https_proxy'
-alias c_google='curl www.google.com'
+alias c_google='curl cip.cc'
 alias cat='bat --style=plain'
 alias l='exa'
 alias la='exa -a'
