474c7f4c188ccf664c973cca2abb6350e2e78205
xiaolitongxue666 <leonpa1987@gamil.com>
2024-10-29 09:40:55 +0800

    update clion cmaketxt create

--- script_tools/clion_cmaketxt_create.sh
+++ script_tools/clion_cmaketxt_create.sh
@@ -1,39 +1,47 @@
+#  Ctrl + Shift + A -> Reload Cmake Project
+
 #!/bin/bash
 
-#Project name is now dir name
+# Function to list directories recursively
+list_dirs_recursively() {
+    local dir="$1"
+    find "$dir" -type d
+}
+
+# Project name is now dir name
 project_path=$(pwd)
 project_name="${project_path##*/}"
 echo "Project name is $project_name"
 
-#CMakeLists.txt update
+# CMakeLists.txt update
 cmake_list_file_path="./CMakeLists.txt"
 
-#Recreate cmke list file
+# Recreate cmke list file
 rm -rf $cmake_list_file_path
 touch $cmake_list_file_path
 
-#Write cmake list file
+# Write cmake list file
 echo -e "cmake_minimum_required(VERSION 3.9)" >> $cmake_list_file_path
 echo -e "project($project_name)" >> $cmake_list_file_path
 echo -e "set(CMAKE_CXX_STANDARD 11)" >> $cmake_list_file_path
 
-#A new line
+# A new line
 echo -e "\n" >> $cmake_list_file_path
 
-#include dirs
-ls_dirs_recursively.sh ./src | awk '{print "include_directories("$0")" }' >> "./include_directories.cmake"
+# Include dirs
+list_dirs_recursively "./src" | awk '{print "include_directories("$0")" }' >> "./include_directories.cmake"
 echo -e "include(./include_directories.cmake)" >> $cmake_list_file_path
 
-#A new line
+# A new line
 echo -e "\n" >> $cmake_list_file_path
 
-#src files
+# Src files
 echo "file(GLOB_RECURSE SOURCES "  >> $cmake_list_file_path
 ls -F | grep "/$" | awk '{print "./"$0"*.c ./"$0"*.h" }' | xargs >> $cmake_list_file_path
 echo ")"  >> $cmake_list_file_path
 
-#A new line
+# A new line
 echo -e "\n" >> $cmake_list_file_path
 
-#Final executable file
+# Final executable file
 echo -e "add_executable($project_name \${SOURCES})" >> $cmake_list_file_path
